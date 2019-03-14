aa78ad41415d7729f1d942bf06c6a4b6f93f3c9f
Nathan Scott <natscott@uat.edu>
2019-03-14 02:00:33 -0700

    Insert function working (at least not throwing errors)

--- HashTables/HashTables/HashNode.h
+++ HashTables/HashTables/HashNode.h
@@ -1,17 +1,37 @@
 #ifndef HASHNODE_H
 #define HASHNODE_H
 
-template <typename KeyType, typename ValueType> class HashNode
+template <typename ValueType> class HashNode
 {
 private:
-	KeyType key;
+	std::string key;
 	ValueType value;
 public:
 	// Default constructor.
-	HashNode(KeyType newKey, ValueType newValue) : key(newKey), value(newValue) { }
+	HashNode(std::string newKey, ValueType newValue) : key(newKey), value(newValue) { }
 
 	// Default destructor.
 	~HashNode() { }
+
+	std::string GetKey()
+	{
+		return key;
+	}
+
+	void SetKey(std::string newKey)
+	{
+		key = newKey;
+	}
+
+	ValueType GetValue()
+	{
+		return value;
+	}
+
+	void SetValue(ValueType newValue)
+	{
+		value = newValue;
+	}
 };
 
 #endif // !HASHNODE_H
--- HashTables/HashTables/HashTable.h
+++ HashTables/HashTables/HashTable.h
@@ -1,6 +1,7 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <string>
 #include <vector>
 
 #include "HashNode.h"
@@ -9,14 +10,26 @@ template <typename T> class HashTable
 {
 private:
 	int tableSize = 0;
-	std::vector<std::vector<HashNode<std::string, T>*>> table;
+
+	// Hashing function.
+	int Hash(std::string key)
+	{
+		long long intKey = 0;
+		for (int charIndex = 0; charIndex < key.size(); charIndex++)
+		{
+			intKey += key.at(charIndex);
+		}
+		return intKey % tableSize;
+	}
 public:
+	std::vector<std::vector<HashNode<T>*>> table;
+
 	// Default constructor.
 	HashTable(int size) : tableSize(size)
 	{
 		for (int count = 0; count < size; count++)
 		{
-			std::vector<HashNode<std::string, T>*> newBucket;
+			std::vector<HashNode<T>*> newBucket;
 			table.push_back(newBucket);
 		}
 	}
@@ -24,7 +37,17 @@ public:
 	// Default destructor.
 	~HashTable() { }
 
+	int Insert(std::string key, T value)
+	{
+		int hashedValue = Hash(key);
+		table[hashedValue].push_back(new HashNode<T>(key, value));
+		return hashedValue;
+	}
 
+	T Retrieve(std::string keyToFind)
+	{
+
+	}
 };
 
 #endif // !HASHTABLE_H
